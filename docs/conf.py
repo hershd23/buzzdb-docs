74eceb0502dffb4fd0cb68fd133d287d5711b50a
Joy Arulraj <arulraj@gatech.edu>
2022-08-16 23:30:59 -0400

    update requirements

--- docs/conf.py
+++ docs/conf.py
@@ -95,41 +95,12 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'mps'
-html_sidebars = {
-    '**': ['localtoc.html', 'relations.html', 'links.html', 'contact.html'],
-}
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-html_theme_path = ['themes']
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# This is required for the alabaster theme
-# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
-
+html_theme = 'furo'
 
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'buzzdbdoc'
-
 
 # -- Options for LaTeX output ---------------------------------------------
 
